e2805f76b84eff1377cf8eea991f60094f6ffe13
Andy Redhead <andy@far2gone.com>
2022-01-03 13:36:14 +0000

    adding original javascript etc from 2021/08

---
+++ www/index.js
@@ -0,0 +1,67 @@
+//let arrow = require("./node_modules/apache-arrow/Arrow");
+import * as arrow from "./node_modules/apache-arrow/Arrow.es2015.min";
+import * as wasm from "read-parquet-browser";
+
+
+async function fetchData() {
+
+    let fileByteArray;
+    try {
+        let fetchResponse = await fetch("./water-stress_rcp26and85_2020-2040-10.parquet");
+        fileByteArray = new Uint8Array(await fetchResponse.arrayBuffer());
+    } catch (fetchErr) {
+        console.error("Fetch error: " + fetchErr);
+    }
+
+    console.log("Parquet data bytelength: " + fileByteArray.byteLength);
+
+    wasm.read_geo_physical_risk_parquet("water-stress", fileByteArray);
+
+}
+
+
+/*
+window.onload = async function() {
+    console.log("window onload called...");
+
+    //wasm.init();
+
+    //await fetchData();
+
+    document
+        .getElementById("button-trigger-read-parquet")
+        .addEventListener("click", async function() {
+                console.log("the button was clicked");
+                await fetchData();
+            });
+
+    console.log("button click event listener added");
+
+    console.log("end window onload.");
+}
+*/
+
+async function main() {
+
+    await fetchData();
+
+    console.log("filtering for specific rcp...");
+    let arrow_result_ipc_msg_bytes = wasm.find_for_rcp("water-stress", 2);
+    console.log("filtering for specific rcp complete, returned buffer byte count: "
+        + arrow_result_ipc_msg_bytes.byteLength);
+
+    try {
+        let record_batch_reader = arrow.RecordBatchStreamReader.from(arrow_result_ipc_msg_bytes);
+        record_batch_reader.open();
+        console.log("schema is: " + record_batch_reader.schema);
+        let result_record_batch = record_batch_reader.next();
+        console.log("result rowcount: " + result_record_batch.value.length);
+    } catch (record_batch_reader_err) {
+        console.log("problem with record_batch_reader: " + record_batch_reader_err);
+    }
+
+}
+
+console.log("trigger fetch data");
+main();
+console.log("end of js");
---
+++ www/webpack.config.js
@@ -0,0 +1,14 @@
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+  entry: "./bootstrap.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bootstrap.js",
+  },
+  mode: "development",
+  plugins: [
+    new CopyWebpackPlugin(['index.html', 'water-stress_rcp26and85_2020-2040-10.parquet'])
+  ],
+};
