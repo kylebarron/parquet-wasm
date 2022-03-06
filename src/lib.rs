bd49ba5294a752f6e95fd561708b2760888ea7a3
Kyle Barron <kylebarron2@gmail.com>
2022-03-06 11:41:21 -0700

    Remove `unwrap`, switch to StreamWriter

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ mod utils;
 
 use js_sys::Uint8Array;
 
-use arrow2::io::ipc::write;
+use arrow2::io::ipc::write::{WriteOptions, StreamWriter};
 // NOTE: It's FileReader on latest main but RecordReader in 0.9.2
 use arrow2::io::parquet::read::FileReader;
 use std::io::Cursor;
@@ -35,45 +35,47 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;*/
 
 #[wasm_bindgen]
 pub fn read_parquet(parquet_file_bytes: &[u8]) -> Result<Uint8Array, JsValue> {
-    log!(
-        "In rust, parquet bytes array has size: {}",
-        parquet_file_bytes.len()
-    );
-
     // Create Parquet reader
     let input_file = Cursor::new(parquet_file_bytes);
-    let file_reader = FileReader::try_new(input_file, None, None, None, None).unwrap();
+    let file_reader = match FileReader::try_new(input_file, None, None, None, None) {
+        Ok(file_reader) => file_reader,
+        Err(error) => return Err(JsValue::from_str(format!("{}", error).as_str())),
+    };
     let schema = file_reader.schema().clone();
 
     // Create IPC writer
     let mut output_file = Vec::new();
-    let options = write::WriteOptions { compression: None };
-    let mut writer = write::FileWriter::try_new(&mut output_file, &schema, None, options).unwrap();
+    let options = WriteOptions { compression: None };
+    let mut writer = StreamWriter::new(&mut output_file, options);
+    match writer.start(&schema, None) {
+        Ok(_) => {}
+        Err(error) => return Err(JsValue::from_str(format!("{}", error).as_str())),
+    }
 
     // Iterate over reader chunks, writing each into the IPC writer
     for maybe_chunk in file_reader {
-        match maybe_chunk {
-            Ok(chunk) => {
-                writer.write(&chunk, None).unwrap();
+        let chunk = match maybe_chunk {
+            Ok(chunk) => chunk,
+            Err(error) => {
+                return Err(JsValue::from_str(format!("{}", error).as_str()));
             }
-            Err(chunk_err) => {
-                return Err(JsValue::from_str(
-                    format!("Failed to read chunk: {}", chunk_err)
-                        .as_str(),
-                ));
-            }
-        }
+        };
+
+        match writer.write(&chunk, None) {
+            Ok(_) => {}
+            Err(error) => return Err(JsValue::from_str(format!("{}", error).as_str())),
+        };
     }
 
-    writer.finish().unwrap();
+    match writer.finish() {
+        Ok(_) => {}
+        Err(error) => return Err(JsValue::from_str(format!("{}", error).as_str())),
+    };
+
     return Ok(unsafe { Uint8Array::view(&output_file) });
 }
 
 #[wasm_bindgen]
 pub fn init() {
-    log!("init - start...");
-
     utils::set_panic_hook();
-
-    log!("init - complete.");
 }
