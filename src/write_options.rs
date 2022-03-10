042180d8de2a795e793e0e176885a91acba8f6c2
Kyle Barron <kylebarron2@gmail.com>
2022-03-09 20:24:53 -0700

    wip

--- src/write_options.rs
+++ src/write_options.rs
@@ -1,3 +1,5 @@
+use js_sys::{Object, Reflect};
+use arrow2::io::parquet::write::{WriteOptions as UpstreamWriteOptions};
 use crate::enums::{Compression, Encoding};
 use wasm_bindgen::prelude::*;
 
@@ -8,13 +10,52 @@ pub struct WriteOptions {
     pub encoding: Encoding,
 }
 
+
 #[wasm_bindgen]
-impl WriteOptions {
-    #[wasm_bindgen(constructor)]
-    pub fn new(compression: Compression, encoding: Encoding) -> Self {
-        Self {
-            compression: compression,
-            encoding: encoding
-        }
+pub struct WriteOptions(arrow2::io::parquet::write::WriteOptions);
+
+#[wasm_bindgen]
+pub fn temp(options: &JsValue) -> Result<(), JsValue> {
+    if (options.is_undefined()) {
+        return Ok(());
     }
+
+    let message: JsValue = Reflect::get(&options, &JsValue::from_str("message"))?;
+    log!("{:?}", message.as_string().unwrap());
+    Ok(())
 }
+
+// #[wasm_bindgen]
+// impl WriteOptions {
+//     #[wasm_bindgen(constructor)]
+//     pub fn new(options: &JsValue) -> Self {
+//         let message: JsValue = Reflect::get(&options, &JsValue::from_str("message"))?;
+
+
+//         options.ok
+//         Self {
+//             write_statistics:
+//             compression: compression,
+//             encoding: encoding
+//         }
+//     }
+// }
+
+// # [wasm_bindgen]
+// pub fn greet(ele: &JsValue, options: &JsValue) -> Result<(), JsValue> {
+//   match ele.dyn_ref::<HtmlDivElement>() {
+//     Some(div) => {
+//       if (options.is_undefined()) {
+//         div.set_inner_text("Hello from Rust");
+//       } else {
+//         let message: JsValue = Reflect::get(&options, &JsValue::from_str("message"))?;
+//         let message = message.as_string().unwrap();
+//         div.set_inner_text(&message);
+//       }
+//       Ok(())
+//     }
+//     None => Err(JsValue::from_str("ele must be a div"))
+//   }
+// }
+
+
