6663d8d5750c9041563e58cd5dd82a5369215611
Kyle Barron <kylebarron2@gmail.com>
2022-03-11 00:21:29 -0700

    Refactor code into lower-level functions, use `?` operator (#25)

--- src/arrow1.rs
+++ src/arrow1.rs
@@ -1,5 +1,17 @@
-use js_sys::Uint8Array;
-use wasm_bindgen::prelude::*;
+#[cfg(feature = "arrow1")]
+use {
+    arrow::ipc::reader::StreamReader,
+    arrow::ipc::writer::StreamWriter,
+    parquet::arrow::arrow_writer::ArrowWriter,
+    parquet::arrow::{ArrowReader, ParquetFileArrowReader},
+    parquet::errors::ParquetError,
+    parquet::file::properties::WriterProperties,
+    parquet::file::reader::{FileReader, SerializedFileReader},
+    parquet::file::serialized_reader::SliceableCursor,
+    parquet::file::writer::InMemoryWriteableCursor,
+    std::io::Cursor,
+    std::sync::Arc,
+};
 
 // A macro to provide `println!(..)`-style syntax for `console.log` logging.
 #[cfg(target_arch = "wasm32")]
@@ -17,22 +29,10 @@ macro_rules! log {
 }
 
 #[cfg(feature = "arrow1")]
-#[wasm_bindgen(js_name = readParquet1)]
-pub fn read_parquet(parquet_file: &[u8]) -> Result<Uint8Array, JsValue> {
-    use js_sys::Uint8Array;
-
-    use arrow::ipc::writer::StreamWriter;
-    use parquet::arrow::{ArrowReader, ParquetFileArrowReader};
-    use parquet::file::reader::{FileReader, SerializedFileReader};
-    use parquet::file::serialized_reader::SliceableCursor;
-    use std::sync::Arc;
-
+pub fn read_parquet(parquet_file: &[u8]) -> Result<Vec<u8>, ParquetError> {
     // Create Parquet reader
     let sliceable_cursor = SliceableCursor::new(Arc::new(parquet_file.to_vec()));
-    let parquet_reader = match SerializedFileReader::new(sliceable_cursor) {
-        Ok(parquet_reader) => parquet_reader,
-        Err(error) => return Err(JsValue::from_str(format!("{}", error).as_str())),
-    };
+    let parquet_reader = SerializedFileReader::new(sliceable_cursor)?;
     let parquet_metadata = parquet_reader.metadata();
     let parquet_file_metadata = parquet_metadata.file_metadata();
     let row_count = parquet_file_metadata.num_rows() as usize;
@@ -41,98 +41,43 @@ pub fn read_parquet(parquet_file: &[u8]) -> Result<Uint8Array, JsValue> {
     let mut arrow_reader = ParquetFileArrowReader::new(Arc::new(parquet_reader));
     // TODO: use Parquet column group row count for arrow record reader row count (i.e. don't read
     // entire file into one IPC batch)
-    let record_batch_reader = match arrow_reader.get_record_reader(row_count) {
-        Ok(record_batch_reader) => record_batch_reader,
-        Err(error) => return Err(JsValue::from_str(format!("{}", error).as_str())),
-    };
-    let arrow_schema = match arrow_reader.get_schema() {
-        Ok(arrow_schema) => arrow_schema,
-        Err(error) => return Err(JsValue::from_str(format!("{}", error).as_str())),
-    };
+    let record_batch_reader = arrow_reader.get_record_reader(row_count)?;
+    let arrow_schema = arrow_reader.get_schema()?;
 
     // Create IPC Writer
     let mut output_file = Vec::new();
-    let mut writer = match StreamWriter::try_new(&mut output_file, &arrow_schema) {
-        Ok(writer) => writer,
-        Err(error) => return Err(JsValue::from_str(format!("{}", error).as_str())),
-    };
+    let mut writer = StreamWriter::try_new(&mut output_file, &arrow_schema)?;
 
+    // Iterate over record batches, writing them to IPC stream
     for maybe_record_batch in record_batch_reader {
-        let record_batch = match maybe_record_batch {
-            Ok(record_batch) => record_batch,
-            Err(error) => return Err(JsValue::from_str(format!("{}", error).as_str())),
-        };
-        match writer.write(&record_batch) {
-            Ok(_) => {}
-            Err(error) => return Err(JsValue::from_str(format!("{}", error).as_str())),
-        };
+        let record_batch = maybe_record_batch?;
+        writer.write(&record_batch)?;
     }
-    match writer.finish() {
-        Ok(_) => {}
-        Err(error) => return Err(JsValue::from_str(format!("{}", error).as_str())),
-    };
+    writer.finish()?;
 
-    let writer_buffer = match writer.into_inner() {
-        Ok(writer_buffer) => writer_buffer,
-        Err(error) => return Err(JsValue::from_str(format!("{}", error).as_str())),
-    };
-    let return_len = match (writer_buffer.len() as usize).try_into() {
-        Ok(return_len) => return_len,
-        Err(error) => return Err(JsValue::from_str(format!("{}", error).as_str())),
-    };
-    let return_vec = Uint8Array::new_with_length(return_len);
-    return_vec.copy_from(&writer_buffer);
-    return Ok(return_vec);
+    let writer_buffer = writer.into_inner()?;
+    return Ok(writer_buffer.to_vec());
 }
 
 #[cfg(feature = "arrow1")]
-#[wasm_bindgen(js_name = writeParquet1)]
-pub fn write_parquet(arrow_file: &[u8]) -> Result<Uint8Array, JsValue> {
-    use arrow::ipc::reader::StreamReader;
-    use parquet::arrow::arrow_writer::ArrowWriter;
-    use parquet::file::properties::WriterProperties;
-    use parquet::file::writer::InMemoryWriteableCursor;
-    use std::io::Cursor;
-
+pub fn write_parquet(arrow_file: &[u8]) -> Result<Vec<u8>, ParquetError> {
     // Create IPC reader
     let input_file = Cursor::new(arrow_file);
-    let arrow_ipc_reader = match StreamReader::try_new(input_file) {
-        Ok(arrow_ipc_reader) => arrow_ipc_reader,
-        Err(error) => return Err(JsValue::from_str(format!("{}", error).as_str())),
-    };
+    let arrow_ipc_reader = StreamReader::try_new(input_file)?;
     let arrow_schema = arrow_ipc_reader.schema();
 
     // Create Parquet writer
     let cursor = InMemoryWriteableCursor::default();
     let props = WriterProperties::builder().build();
-    let mut writer = match ArrowWriter::try_new(cursor.clone(), arrow_schema, Some(props)) {
-        Ok(writer) => writer,
-        Err(error) => return Err(JsValue::from_str(format!("{}", error).as_str())),
-    };
+    let mut writer = ArrowWriter::try_new(cursor.clone(), arrow_schema, Some(props))?;
 
     // Iterate over IPC chunks, writing each batch to Parquet
     for maybe_record_batch in arrow_ipc_reader {
-        let record_batch = match maybe_record_batch {
-            Ok(record_batch) => record_batch,
-            Err(error) => return Err(JsValue::from_str(format!("{}", error).as_str())),
-        };
-
-        match writer.write(&record_batch) {
-            Ok(_) => {}
-            Err(error) => return Err(JsValue::from_str(format!("{}", error).as_str())),
-        };
+        let record_batch = maybe_record_batch?;
+        writer.write(&record_batch)?;
     }
-    match writer.close() {
-        Ok(_) => {}
-        Err(error) => return Err(JsValue::from_str(format!("{}", error).as_str())),
-    };
 
-    let return_buffer = cursor.data();
-    let return_len = match (return_buffer.len() as usize).try_into() {
-        Ok(return_len) => return_len,
-        Err(error) => return Err(JsValue::from_str(format!("{}", error).as_str())),
-    };
-    let return_vec = Uint8Array::new_with_length(return_len);
-    return_vec.copy_from(&return_buffer);
-    return Ok(return_vec);
+    writer.close()?;
+
+    return Ok(cursor.data());
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ mod arrow1;
 mod arrow2;
 mod utils;
 
+use js_sys::Uint8Array;
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -11,6 +12,78 @@ use wasm_bindgen::prelude::*;
 /*#[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;*/
+
+#[cfg(feature = "arrow1")]
+#[wasm_bindgen(js_name = readParquet1)]
+pub fn read_parquet1(parquet_file: &[u8]) -> Result<Uint8Array, JsValue> {
+    let buffer = match crate::arrow1::read_parquet(parquet_file) {
+        // This function would return a rust vec that would be copied to a Uint8Array here
+        Ok(buffer) => buffer,
+        Err(error) => return Err(JsValue::from_str(format!("{}", error).as_str())),
+    };
+
+    let return_len = match (buffer.len() as usize).try_into() {
+        Ok(return_len) => return_len,
+        Err(error) => return Err(JsValue::from_str(format!("{}", error).as_str())),
+    };
+    let return_vec = Uint8Array::new_with_length(return_len);
+    return_vec.copy_from(&buffer);
+    return Ok(return_vec);
+}
+
+#[cfg(feature = "arrow1")]
+#[wasm_bindgen(js_name = writeParquet1)]
+pub fn write_parquet1(arrow_file: &[u8]) -> Result<Uint8Array, JsValue> {
+    let buffer = match crate::arrow1::write_parquet(arrow_file) {
+        // This function would return a rust vec that would be copied to a Uint8Array here
+        Ok(buffer) => buffer,
+        Err(error) => return Err(JsValue::from_str(format!("{}", error).as_str())),
+    };
+
+    let return_len = match (buffer.len() as usize).try_into() {
+        Ok(return_len) => return_len,
+        Err(error) => return Err(JsValue::from_str(format!("{}", error).as_str())),
+    };
+    let return_vec = Uint8Array::new_with_length(return_len);
+    return_vec.copy_from(&buffer);
+    return Ok(return_vec);
+}
+
+#[cfg(feature = "arrow2")]
+#[wasm_bindgen(js_name = readParquet2)]
+pub fn read_parquet2(parquet_file: &[u8]) -> Result<Uint8Array, JsValue> {
+    let buffer = match crate::arrow2::read_parquet(parquet_file) {
+        // This function would return a rust vec that would be copied to a Uint8Array here
+        Ok(buffer) => buffer,
+        Err(error) => return Err(JsValue::from_str(format!("{}", error).as_str())),
+    };
+
+    let return_len = match (buffer.len() as usize).try_into() {
+        Ok(return_len) => return_len,
+        Err(error) => return Err(JsValue::from_str(format!("{}", error).as_str())),
+    };
+    let return_vec = Uint8Array::new_with_length(return_len);
+    return_vec.copy_from(&buffer);
+    return Ok(return_vec);
+}
+
+#[cfg(feature = "arrow2")]
+#[wasm_bindgen(js_name = writeParquet2)]
+pub fn write_parquet2(arrow_file: &[u8]) -> Result<Uint8Array, JsValue> {
+    let buffer = match crate::arrow2::write_parquet(arrow_file) {
+        // This function would return a rust vec that would be copied to a Uint8Array here
+        Ok(buffer) => buffer,
+        Err(error) => return Err(JsValue::from_str(format!("{}", error).as_str())),
+    };
+
+    let return_len = match (buffer.len() as usize).try_into() {
+        Ok(return_len) => return_len,
+        Err(error) => return Err(JsValue::from_str(format!("{}", error).as_str())),
+    };
+    let return_vec = Uint8Array::new_with_length(return_len);
+    return_vec.copy_from(&buffer);
+    return Ok(return_vec);
+}
 
 #[wasm_bindgen(js_name = setPanicHook)]
 pub fn set_panic_hook() {
