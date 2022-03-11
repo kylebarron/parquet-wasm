62fce92772854f8a3310dbdb87be652debbb1495
Kyle Barron <kbarron@foursquare.com>
2022-03-10 18:30:00 -0700

    Don't store intermediate record batches in vector

--- src/arrow1.rs
+++ src/arrow1.rs
@@ -18,7 +18,7 @@ macro_rules! log {
 
 #[cfg(feature = "arrow1")]
 #[wasm_bindgen(js_name = readParquet1)]
-pub fn read_parquet1(parquet_file: &[u8]) -> Result<Uint8Array, JsValue> {
+pub fn read_parquet(parquet_file: &[u8]) -> Result<Uint8Array, JsValue> {
     use js_sys::Uint8Array;
 
     use arrow::error::ArrowError;
@@ -29,40 +29,42 @@ pub fn read_parquet1(parquet_file: &[u8]) -> Result<Uint8Array, JsValue> {
     use parquet::file::serialized_reader::SliceableCursor;
     use std::sync::Arc;
 
-    let parquet_bytes_as_vec = parquet_file.to_vec();
-    let parquet_vec_arc = Arc::new(parquet_bytes_as_vec);
-    let sliceable_cursor = SliceableCursor::new(parquet_vec_arc);
-
-    let parquet_reader = SerializedFileReader::new(sliceable_cursor).unwrap();
+    // Create Parquet reader
+    let sliceable_cursor = SliceableCursor::new(Arc::new(parquet_file.to_vec()));
+    let parquet_reader = match SerializedFileReader::new(sliceable_cursor) {
+        Ok(parquet_reader) => parquet_reader,
+        Err(error) => return Err(JsValue::from_str(format!("{}", error).as_str())),
+    };
     let parquet_metadata = parquet_reader.metadata();
     let parquet_file_metadata = parquet_metadata.file_metadata();
     let row_count = parquet_file_metadata.num_rows() as usize;
 
+    // Create Arrow reader from Parquet reader
     let mut arrow_reader = ParquetFileArrowReader::new(Arc::new(parquet_reader));
-    let record_batch_reader = arrow_reader.get_record_reader(row_count).unwrap();
+    // TODO: use Parquet column group row count for arrow record reader row count (i.e. don't read
+    // entire file)
+    let record_batch_reader = match arrow_reader.get_record_reader(row_count) {
+        Ok(record_batch_reader) => record_batch_reader,
+        Err(error) => return Err(JsValue::from_str(format!("{}", error).as_str())),
+    };
     let arrow_schema = arrow_reader.get_schema().unwrap();
 
-    let mut record_batches: Vec<RecordBatch> = Vec::new();
+    // Create IPC Writer
+    let mut output_file = Vec::new();
+    let mut writer = FileWriter::try_new(&mut output_file, &arrow_schema).unwrap();
+
     for maybe_record_batch in record_batch_reader {
-        let record_batch = maybe_record_batch.unwrap();
-        record_batches.push(record_batch);
+        let record_batch = match maybe_record_batch {
+            Ok(record_batch) => record_batch,
+            Err(error) => return Err(JsValue::from_str(format!("{}", error).as_str())),
+        };
+        writer.write(&record_batch).unwrap();
     }
+    writer.finish().unwrap();
+    let buf = writer.into_inner().unwrap();
 
-    let mut file = Vec::new();
-    {
-        let mut writer = FileWriter::try_new(&mut file, &arrow_schema).unwrap();
-
-        let result: Result<Vec<()>, ArrowError> = record_batches
-            .iter()
-            .map(|batch| writer.write(batch))
-            .collect();
-        result.unwrap();
-        writer.finish().unwrap();
-    }
-
-    log!("{:?}", &file);
-    let return_vec = Uint8Array::new_with_length((file.len() as usize).try_into().unwrap());
-    return_vec.copy_from(&file);
-
+    let return_len = (buf.len() as usize).try_into().unwrap();
+    let return_vec = Uint8Array::new_with_length(return_len);
+    return_vec.copy_from(&buf);
     return Ok(return_vec);
 }
