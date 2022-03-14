f68887fcee9ca0d8f093caa00c3b6319b7fd89af
Kyle Barron <kylebarron2@gmail.com>
2022-03-13 23:03:33 -0600

    Move test data files

--- tests/data/generate_data.py
+++ tests/data/generate_data.py
@@ -2,7 +2,7 @@ import pyarrow as pa
 import pyarrow.feather as feather
 import pyarrow.parquet as pq
 
-compressions = ["SNAPPY", "GZIP", "BROTLI", "LZ4", "ZSTD", 'NONE']
+compressions = ["SNAPPY", "GZIP", "BROTLI", "LZ4", "ZSTD", "NONE"]
 
 
 def create_data():
@@ -35,19 +35,5 @@ def main():
     write_data(table)
 
 
-def debug():
-    """Create debug data"""
-    works = pa.table({
-        "uint8": pa.array([1, 2, 3, 4], type=pa.uint8()),
-    })
-    pq.write_table(works, 'works.parquet', compression='NONE')
-
-    not_work = pa.table({
-        "int8": pa.array([0, 0, 0, 0], type=pa.int8()),
-    })
-    pq.write_table(not_work, 'not_work.parquet', compression='NONE')
-
-
 if __name__ == "__main__":
     main()
-    debug()
