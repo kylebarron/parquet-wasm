e420bf25aac787a56c650425282915bc0afff748
Kyle Barron <kylebarron2@gmail.com>
2022-04-08 22:44:13 -0600

    WIP

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,69 +6,81 @@ mkdir -p tmp_build
 ######################################
 # ARROW 1 (arrow-rs) the default feature
 # Build node version into tmp_build/node
-echo "Building arrow-rs node"
-wasm-pack build \
-  --release \
-  --out-dir tmp_build/node \
-  --out-name arrow1 \
-  --target nodejs
+if [[ -n "$SKIP_NODE" ]]; then
+  echo "Building arrow-rs node"
+  wasm-pack build \
+    --release \
+    --out-dir tmp_build/node \
+    --out-name arrow1 \
+    --target nodejs
+fi
 
 # Build web version into tmp_build/esm
-echo "Building arrow-rs esm"
-wasm-pack build \
-  --release \
-  --out-dir tmp_build/esm \
-  --out-name arrow1 \
-  --target web
+if [[ -n "$SKIP_ESM" ]]; then
+  echo "Building arrow-rs esm"
+  wasm-pack build \
+    --release \
+    --out-dir tmp_build/esm \
+    --out-name arrow1 \
+    --target web
+fi
 
 # Build bundler version into tmp_build/bundler
-echo "Building arrow-rs bundler"
-wasm-pack build \
-  --release \
-  --out-dir tmp_build/bundler \
-  --out-name arrow1 \
-  --target bundler
+if [[ -n "$SKIP_BUNDLER" ]]; then
+  echo "Building arrow-rs bundler"
+  wasm-pack build \
+    --release \
+    --out-dir tmp_build/bundler \
+    --out-name arrow1 \
+    --target bundler
+fi
 
 ######################################
 # ARROW 2 turn on the feature manually
 # Build node version into tmp_build/node2
-echo "Building arrow2 node"
-wasm-pack build \
-  --release \
-  --out-dir tmp_build/node2 \
-  --out-name arrow2 \
-  --target nodejs \
-  --no-default-features \
-  --features arrow2 \
-  --features reader \
-  --features writer \
-  --features parquet2_supported_compressions
+if [[ -n "$SKIP_NODE" ]]; then
+  echo "Building arrow2 node"
+  wasm-pack build \
+    --release \
+    --out-dir tmp_build/node2 \
+    --out-name arrow2 \
+    --target nodejs \
+    --no-default-features \
+    --features arrow2 \
+    --features reader \
+    --features writer \
+    --features parquet2_supported_compressions
+fi
 
 # Build web version into tmp_build/esm2
-echo "Building arrow2 esm"
-wasm-pack build \
-  --release \
-  --out-dir tmp_build/esm2 \
-  --out-name arrow2 \
-  --target web \
-  --no-default-features \
-  --features arrow2 \
-  --features reader \
-  --features writer \
-  --features parquet2_supported_compressions
+if [[ -n "$SKIP_ESM" ]]; then
+  echo "Building arrow2 esm"
+  wasm-pack build \
+    --release \
+    --out-dir tmp_build/esm2 \
+    --out-name arrow2 \
+    --target web \
+    --no-default-features \
+    --features arrow2 \
+    --features reader \
+    --features writer \
+    --features parquet2_supported_compressions
+fi
 
 # Build bundler version into tmp_build/bundler2
-echo "Building arrow2 bundler"
-wasm-pack build \
-  --release \
-  --out-dir tmp_build/bundler2 \
-  --out-name arrow2 \
-  --target bundler \
-  --no-default-features \
-  --features arrow2 \
-  --features reader \
-  --features writer \
-  --features parquet2_supported_compressions
+if [[ -n "$SKIP_BUNDLER" ]]; then
+  echo "Building arrow2 bundler"
+  wasm-pack build \
+    --release \
+    --out-dir tmp_build/bundler2 \
+    --out-name arrow2 \
+    --target bundler \
+    --no-default-features \
+    --features arrow2 \
+    --features reader \
+    --features writer \
+    --features parquet2_supported_compressions
+fi
 
 # Copy files into pkg/
 mkdir -p pkg/{node,esm,bundler}
