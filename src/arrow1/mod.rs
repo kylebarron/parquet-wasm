96d425d0e7d34946f5ff186c1d656c607f2c3519
Kyle Barron <kylebarron2@gmail.com>
2022-04-24 13:20:22 -0600

    copy into arrow1

--- src/arrow1/mod.rs
+++ src/arrow1/mod.rs
@@ -1,6 +1,12 @@
 #[cfg(feature = "reader")]
 pub mod reader;
 
+#[cfg(feature = "reader")]
+pub mod reader_async;
+
+#[cfg(feature = "reader")]
+pub mod ranged_reader;
+
 pub mod wasm;
 
 #[cfg(feature = "writer")]
