f0bec272c403cc71d783fb7e172dc6f46aba38c4
Kyle Barron <kbarron@foursquare.com>
2022-04-25 19:34:47 -0600

    Check that input exists

--- src/arrow1/wasm.rs
+++ src/arrow1/wasm.rs
@@ -23,6 +23,10 @@ use wasm_bindgen::prelude::*;
 #[wasm_bindgen(js_name = readParquet)]
 #[cfg(feature = "reader")]
 pub fn read_parquet(parquet_file: &[u8]) -> Result<Uint8Array, JsValue> {
+    if parquet_file.len() == 0 {
+        return Err(JsValue::from_str("Empty input provided or not a Uint8Array."));
+    }
+
     match crate::arrow1::reader::read_parquet(parquet_file) {
         Ok(buffer) => copy_vec_to_uint8_array(buffer),
         Err(error) => return Err(JsValue::from_str(format!("{}", error).as_str())),
--- src/arrow2/wasm.rs
+++ src/arrow2/wasm.rs
@@ -23,6 +23,10 @@ use wasm_bindgen::prelude::*;
 #[wasm_bindgen(js_name = readParquet2)]
 #[cfg(feature = "reader")]
 pub fn read_parquet2(parquet_file: &[u8]) -> Result<Uint8Array, JsValue> {
+    if parquet_file.len() == 0 {
+        return Err(JsValue::from_str("Empty input provided or not a Uint8Array."));
+    }
+
     match crate::arrow2::reader::read_parquet(parquet_file) {
         Ok(buffer) => copy_vec_to_uint8_array(buffer),
         Err(error) => Err(JsValue::from_str(format!("{}", error).as_str())),
