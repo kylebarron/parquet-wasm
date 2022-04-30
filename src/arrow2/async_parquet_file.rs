55d0447cf9e3d23f7383bd4521cd38f59bcd289c
Kyle Barron <kylebarron2@gmail.com>
2022-04-30 15:15:29 -0600

    Working async read of a row group!

--- src/arrow2/async_parquet_file.rs
+++ src/arrow2/async_parquet_file.rs
@@ -3,8 +3,25 @@ use crate::arrow2::reader_async::{create_reader, read_parquet_metadata_async};
 use crate::fetch::get_content_length;
 use arrow2::datatypes::Schema;
 use arrow2::io::parquet::read::FileMetaData;
-use arrow2::io::parquet::read::{infer_schema, read_metadata_async};
+use arrow2::io::parquet::read::{
+    infer_schema, read_columns_async, read_columns_many_async, read_metadata_async,
+    RowGroupDeserializer,
+};
+use futures::channel::{mpsc, oneshot};
 use wasm_bindgen::prelude::*;
+use wasm_bindgen_futures::spawn_local;
+
+use crate::fetch::make_range_request;
+use crate::log;
+use arrow2::error::ArrowError;
+use arrow2::error::Result as ArrowResult;
+use arrow2::io::ipc::write::{StreamWriter as IPCStreamWriter, WriteOptions as IPCWriteOptions};
+use futures::future::BoxFuture;
+// NOTE: It's FileReader on latest main but RecordReader in 0.9.2
+use arrow2::io::parquet::read::FileReader as ParquetFileReader;
+use std::io::Cursor;
+use crate::utils::copy_vec_to_uint8_array;
+use js_sys::Uint8Array;
 
 /// Asynchronous implementation of ParquetFile
 #[wasm_bindgen]
@@ -54,5 +71,94 @@ impl AsyncParquetFile {
     #[wasm_bindgen]
     pub fn column_name(&self, field: usize) -> String {
         self.schema.fields[field].name.clone()
+    }
+
+    #[wasm_bindgen]
+    pub fn num_row_groups(&self) -> usize {
+        self.metadata.row_groups.len()
+    }
+
+    pub async fn read_row_group(self, i: usize) -> Result<Uint8Array, JsValue> {
+        // let (mpsc_sender, mpsc_receiver) = mpsc::channel::<Vec<u8>>(100);
+        let url = self.url.clone();
+
+        let range_get = Box::new(move |start: u64, length: usize| {
+            let url = url.clone();
+            // let local_mpsc_sender = mpsc_sender.clone();
+
+            let future_fn = Box::pin(async move {
+                let (local_oneshot_sender, local_oneshot_receiver) = oneshot::channel::<Vec<u8>>();
+                spawn_local(async move {
+                    log!("Making range request");
+                    let inner_data = make_range_request(url, start, length).await.unwrap();
+                    // local_mpsc_sender.send(inner_data);
+                    local_oneshot_sender.send(inner_data);
+                });
+                let data = local_oneshot_receiver.await.unwrap();
+
+                Ok(RangeOutput { start, data })
+            }) as BoxFuture<'static, std::io::Result<RangeOutput>>;
+
+            future_fn
+        });
+
+        let min_request_size = 4 * 1024;
+
+        // let reader_factory1 = Box::new(|| {
+        //     Box::pin(futures::future::ready({})
+
+        //         async move {
+        //         let (sender2, receiver2) = oneshot::channel::<Vec<u8>>();
+        //         spawn_local(async move {
+        //             log!("Making range request");
+        //             let inner_data = make_range_request(url, start, length).await.unwrap();
+        //             sender2.send(inner_data).unwrap();
+        //         });
+        //         let data = receiver2.await.unwrap();
+
+        //         Ok(RangeOutput { start, data })
+        //     }) as BoxFuture<'static, std::io::Result<RangeOutput>>
+        // });
+
+        let reader_factory = || {
+            Box::pin(futures::future::ready(Ok(RangedAsyncReader::new(
+                self.content_length,
+                min_request_size,
+                range_get.clone(),
+            ))))
+                as BoxFuture<'static, std::result::Result<RangedAsyncReader, std::io::Error>>
+        };
+
+        // let's read the first row group only. Iterate over them to your liking
+        let group = &self.metadata.row_groups[0];
+
+        // no chunk size in deserializing
+        let chunk_size = None;
+
+        let fields = self.schema.fields.clone();
+
+        // this is IO-bounded (and issues a join, thus the reader_factory)
+        let column_chunks = read_columns_many_async(reader_factory, group, fields, chunk_size)
+            .await
+            .unwrap();
+
+        // Create IPC writer
+        let mut output_file = Vec::new();
+        let options = IPCWriteOptions { compression: None };
+        let mut writer = IPCStreamWriter::new(&mut output_file, options);
+        writer.start(&self.schema, None).unwrap();
+
+        // this is CPU-bounded and should be sent to a separate thread-pool.
+        // We do it here for simplicity
+        let chunks = RowGroupDeserializer::new(column_chunks, group.num_rows() as usize, None);
+        let chunks = chunks.collect::<ArrowResult<Vec<_>>>().unwrap();
+        for chunk in chunks {
+            // let chunk2 = chunk;
+            writer.write(&chunk, None);
+        }
+
+        writer.finish().unwrap();
+        let array = copy_vec_to_uint8_array(output_file).unwrap();
+        Ok(array)
     }
 }
--- www/async_testing.js
+++ www/async_testing.js
@@ -1,3 +1,4 @@
+import * as arrow from "@apache-arrow/es2015-cjs/Arrow.dom";
 import * as wasm from "parquet-wasm";
 
 wasm.setPanicHook();
@@ -8,9 +9,14 @@ window.url = url;
 async function main() {
   console.log('hello world');
   const parquetFile = await new wasm.AsyncParquetFile(url);
-  window.parquetFile = parquetFile;
   console.log('parquetFile', parquetFile);
   console.log('content length', parquetFile.content_length());
+  window.parquetFile = parquetFile;
+  const arrow_ipc_bytes = await parquetFile.read_row_group(0);
+
+  const table = arrow.tableFromIPC(arrow_ipc_bytes);
+  window.table = table;
+  console.log("table", table);
 
   // await wasm.readParquet
   // wasm.
