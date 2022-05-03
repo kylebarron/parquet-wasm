04de6188c747316758347b89e634c485da3e33ce
Kyle Barron <kylebarron2@gmail.com>
2022-05-02 18:04:36 -0600

    Simplify cargo features (#117)

--- scripts/build.sh
+++ scripts/build.sh
@@ -47,7 +47,7 @@ wasm-pack build \
   --features arrow2 \
   --features reader \
   --features writer \
-  --features parquet2_supported_compressions
+  --features all_compressions
 
 # Build web version into tmp_build/esm2
 echo "Building arrow2 esm"
@@ -60,7 +60,7 @@ wasm-pack build \
   --features arrow2 \
   --features reader \
   --features writer \
-  --features parquet2_supported_compressions
+  --features all_compressions
 
 # Build bundler version into tmp_build/bundler2
 echo "Building arrow2 bundler"
@@ -73,7 +73,7 @@ wasm-pack build \
   --features arrow2 \
   --features reader \
   --features writer \
-  --features parquet2_supported_compressions
+  --features all_compressions
 
 # Copy files into pkg/
 mkdir -p pkg/{node,esm,bundler}
@@ -87,6 +87,7 @@ cp tmp_build/bundler/{package.json,LICENSE_APACHE,LICENSE_MIT,README.md} pkg/
 # Create minimal package.json in esm/ folder with type: module
 echo '{"type": "module"}' > pkg/esm/package.json
 
+# Create an esm2/ directory without import.meta.url in the JS ESM wrapper
 cp -r pkg/esm pkg/esm2
 sed '/import.meta.url/ s|input|// input|' pkg/esm2/arrow1.js > pkg/esm2/arrow1_new.js
 sed '/import.meta.url/ s|input|// input|' pkg/esm2/arrow2.js > pkg/esm2/arrow2_new.js
