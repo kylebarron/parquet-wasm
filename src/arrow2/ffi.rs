681cf6216fdde58ae6d265a3478386d8c6b78e9a
Kyle Barron <kylebarron2@gmail.com>
2022-08-13 21:54:30 -0600

    working import?

--- src/arrow2/ffi.rs
+++ src/arrow2/ffi.rs
@@ -1,15 +1,25 @@
+use crate::arrow2::error::Result;
 use arrow2::array::Array;
 use arrow2::chunk::Chunk;
-use arrow2::datatypes::{Field, Schema};
+use arrow2::datatypes::{DataType, Field, Schema};
 use arrow2::ffi;
 use wasm_bindgen::prelude::*;
 
+use super::error::ParquetWasmError;
+
 #[wasm_bindgen]
-pub struct FFIArrowArray(ffi::ArrowArray);
+pub struct FFIArrowArray(Box<ffi::ArrowArray>);
 
 impl From<Box<dyn Array>> for FFIArrowArray {
     fn from(array: Box<dyn Array>) -> Self {
-        Self(ffi::export_array_to_c(array))
+        Self(Box::new(ffi::export_array_to_c(array)))
+    }
+}
+
+impl FFIArrowArray {
+    fn import(self, data_type: DataType) -> Result<Box<dyn Array>> {
+        let imported = unsafe { ffi::import_array_from_c(*self.0, data_type) };
+        Ok(imported?)
     }
 }
 
@@ -17,7 +27,7 @@ impl From<Box<dyn Array>> for FFIArrowArray {
 impl FFIArrowArray {
     #[wasm_bindgen]
     pub fn addr(&self) -> *const ffi::ArrowArray {
-        &self.0 as *const _
+        self.0.as_ref() as *const _
     }
 
     #[wasm_bindgen]
@@ -32,11 +42,20 @@ impl FFIArrowArray {
 }
 
 #[wasm_bindgen]
-pub struct FFIArrowField(ffi::ArrowSchema);
+pub struct FFIArrowField(Box<ffi::ArrowSchema>);
 
 impl From<&Field> for FFIArrowField {
     fn from(field: &Field) -> Self {
-        Self(ffi::export_field_to_c(field))
+        Self(Box::new(ffi::export_field_to_c(field)))
+    }
+}
+
+impl TryFrom<&FFIArrowField> for arrow2::datatypes::Field {
+    type Error = ParquetWasmError;
+
+    fn try_from(field: &FFIArrowField) -> Result<Self> {
+        let imported = unsafe { ffi::import_field_from_c(&field.0) };
+        Ok(imported?)
     }
 }
 
@@ -44,7 +63,7 @@ impl From<&Field> for FFIArrowField {
 impl FFIArrowField {
     #[wasm_bindgen]
     pub fn addr(&self) -> *const ffi::ArrowSchema {
-        &self.0 as *const _
+        self.0.as_ref() as *const _
     }
 }
 
@@ -57,6 +76,29 @@ impl From<Chunk<Box<dyn Array>>> for FFIArrowChunk {
         let ffi_arrays: Vec<FFIArrowArray> =
             chunk.iter().map(|array| array.clone().into()).collect();
         Self(ffi_arrays)
+    }
+}
+
+impl FFIArrowChunk {
+    // NOTE: this todo may no longer apply:
+
+    // TODO: The idea was to try and import data via FFI as well to make sure that the exported
+    // data can _in principle_ be read back if you figure out the C ABI correctly
+    // One issue here is that since the FFI structs don't support `copy`, you have to _consume_
+    // this and similar structs when you import. E.g. take `self` instead of `&self`.
+    // Then the next issue is in returning an object whose size is not known at compile time.
+    // If we consume the struct, we need to import _all_ the data via FFI. But then we need to
+    // return a Chunk or Vec of arrays... but those aren't all known at compile time. Do we need to
+    // box it?
+    //
+    // It would probably be good to see how Polars solves this problem.
+    pub fn import(self, data_types: &Vec<&DataType>) -> Result<Chunk<Box<dyn Array>>> {
+        let mut arrays: Vec<Box<dyn Array>> = vec![];
+        for (i, ffi_array) in self.0.into_iter().enumerate() {
+            arrays.push(ffi_array.import(data_types[i].clone())?);
+        }
+
+        Ok(Chunk::new(arrays))
     }
 }
 
@@ -84,6 +126,26 @@ impl From<&Schema> for FFIArrowSchema {
     }
 }
 
+impl FFIArrowSchema {
+    pub fn import(&self, i: usize) -> Result<Field> {
+        let ffi_arrow_field = &self.0[i];
+        ffi_arrow_field.try_into()
+    }
+}
+
+impl TryFrom<&FFIArrowSchema> for Schema {
+    type Error = ParquetWasmError;
+
+    fn try_from(schema: &FFIArrowSchema) -> Result<Self> {
+        let mut fields: Vec<Field> = vec![];
+        for i in 0..schema.length() {
+            fields.push(schema.import(i)?);
+        }
+
+        Ok(fields.into())
+    }
+}
+
 #[wasm_bindgen]
 impl FFIArrowSchema {
     #[wasm_bindgen]
@@ -99,13 +161,16 @@ impl FFIArrowSchema {
 
 #[wasm_bindgen]
 pub struct FFIArrowTable {
-    schema: FFIArrowSchema,
+    schema: Box<FFIArrowSchema>,
     chunks: Vec<FFIArrowChunk>,
 }
 
 impl From<(FFIArrowSchema, Vec<FFIArrowChunk>)> for FFIArrowTable {
     fn from((schema, chunks): (FFIArrowSchema, Vec<FFIArrowChunk>)) -> Self {
-        Self { schema, chunks }
+        Self {
+            schema: Box::new(schema),
+            chunks,
+        }
     }
 }
 
@@ -140,5 +205,24 @@ impl FFIArrowTable {
     pub fn drop(self) {
         drop(self.schema);
         drop(self.chunks);
+    }
+}
+
+impl FFIArrowTable {
+    pub fn import(self) -> Result<Vec<Chunk<Box<dyn Array>>>> {
+        let schema: Schema = self.schema.as_ref().try_into()?;
+        let data_types: Vec<&DataType> = schema
+            .fields
+            .iter()
+            .map(|field| field.data_type())
+            .collect();
+
+        let mut chunks: Vec<Chunk<Box<dyn Array>>> = vec![];
+        for chunk in self.chunks.into_iter() {
+            let imported = chunk.import(&data_types)?;
+            chunks.push(imported);
+        }
+
+        Ok(chunks)
     }
 }
