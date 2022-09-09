1c78870913f108bc7466c1961bf3693586c330ad
Kyle Barron <kylebarron2@gmail.com>
2022-09-09 13:04:07 +0200

    Use reqwest for async apis (attempt 2) (#203)

--- src/common/fetch.rs
+++ src/common/fetch.rs
@@ -1,27 +1,14 @@
+use std::convert::TryInto;
+
 use futures::channel::oneshot;
 use wasm_bindgen::prelude::*;
-use wasm_bindgen::JsCast;
-use wasm_bindgen_futures::JsFuture;
-use web_sys::{Request, RequestInit, RequestMode, Response};
-
 use wasm_bindgen_futures::spawn_local;
 
 /// Get content-length of file
-pub async fn _get_content_length(url: String) -> Result<usize, JsValue> {
-    let mut opts = RequestInit::new();
-    opts.method("HEAD");
-    opts.mode(RequestMode::Cors);
-
-    let request = Request::new_with_str_and_init(&url, &opts)?;
-    let window = web_sys::window().unwrap();
-
-    let resp_value = JsFuture::from(window.fetch_with_request(&request)).await?;
-    let resp: Response = resp_value.dyn_into().unwrap();
-
-    let length = resp.headers().get("content-length")?;
-    let a = length.unwrap();
-    let length_int = a.parse::<usize>().unwrap();
-    Ok(length_int)
+pub async fn _get_content_length(url: String) -> Result<usize, reqwest::Error> {
+    let client = reqwest::Client::new();
+    let resp = client.head(url).send().await?;
+    Ok(resp.content_length().unwrap().try_into().unwrap())
 }
 
 pub async fn get_content_length(url: String) -> Result<usize, JsValue> {
@@ -49,22 +36,20 @@ pub fn range_from_end(length: u64) -> String {
 }
 
 /// Make range request on remote file
-async fn _make_range_request(url: &str, start: u64, length: usize) -> Result<Vec<u8>, JsValue> {
-    let mut opts = RequestInit::new();
-    opts.method("GET");
-    opts.mode(RequestMode::Cors);
-
-    let request = Request::new_with_str_and_init(url, &opts)?;
-
-    request
-        .headers()
-        .set("Range", &range_from_start_and_length(start, length as u64))?;
-
-    let window = web_sys::window().unwrap();
-    let resp_value = JsFuture::from(window.fetch_with_request(&request)).await?;
-    let resp: Response = resp_value.dyn_into().unwrap();
-
-    Ok(resp_into_bytes(resp).await)
+async fn _make_range_request(
+    url: &str,
+    start: u64,
+    length: usize,
+) -> Result<Vec<u8>, reqwest::Error> {
+    let client = reqwest::Client::new();
+    let range_str = range_from_start_and_length(start, length as u64);
+    let resp = client
+        .get(url)
+        .header("Range", range_str)
+        .send()
+        .await?
+        .error_for_status()?;
+    Ok(resp.bytes().await?.to_vec())
 }
 
 pub async fn make_range_request(
@@ -79,13 +64,4 @@ pub async fn make_range_request(
     });
     let data = receiver.await.unwrap();
     Ok(data)
-}
-
-async fn resp_into_bytes(resp: Response) -> Vec<u8> {
-    let array_buffer_promise = JsFuture::from(resp.array_buffer().unwrap());
-    let array_buffer: JsValue = array_buffer_promise
-        .await
-        .expect("Could not get ArrayBuffer from file");
-
-    js_sys::Uint8Array::new(&array_buffer).to_vec()
 }
