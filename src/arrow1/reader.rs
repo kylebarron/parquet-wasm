d6656194676f23fdd241817d4d0243534f117cbf
Kyle Barron <kylebarron2@gmail.com>
2023-08-07 12:15:45 -0400

    Remove memory copy (#280)

--- src/arrow1/reader.rs
+++ src/arrow1/reader.rs
@@ -16,14 +16,19 @@ pub fn read_parquet(parquet_file: Vec<u8>) -> Result<Vec<u8>> {
 
     // Create IPC Writer
     let mut output_file = Vec::new();
-    let mut writer = StreamWriter::try_new(&mut output_file, &arrow_schema)?;
 
-    // Iterate over record batches, writing them to IPC stream
-    for maybe_record_batch in reader {
-        let record_batch = maybe_record_batch?;
-        writer.write(&record_batch)?;
+    {
+        let mut writer = StreamWriter::try_new(&mut output_file, &arrow_schema)?;
+
+        // Iterate over record batches, writing them to IPC stream
+        for maybe_record_batch in reader {
+            let record_batch = maybe_record_batch?;
+            writer.write(&record_batch)?;
+        }
+        writer.finish()?;
     }
-    writer.finish()?;
 
-    Ok(writer.into_inner()?.to_vec())
+    // Note that this returns output_file directly instead of using writer.into_inner().to_vec() as
+    // the latter seems likely to incur an extra copy of the vec
+    Ok(output_file)
 }
--- src/arrow1/wasm.rs
+++ src/arrow1/wasm.rs
@@ -1,6 +1,5 @@
 use crate::arrow1::error::WasmResult;
-use crate::utils::{assert_parquet_file_not_empty, copy_vec_to_uint8_array};
-use js_sys::Uint8Array;
+use crate::utils::assert_parquet_file_not_empty;
 use wasm_bindgen::prelude::*;
 
 /// Read a Parquet file into Arrow data using the [`arrow`](https://crates.io/crates/arrow) and
@@ -23,11 +22,9 @@ use wasm_bindgen::prelude::*;
 /// @returns Uint8Array containing Arrow data in [IPC Stream format](https://arrow.apache.org/docs/format/Columnar.html#ipc-streaming-format). To parse this into an Arrow table, pass to `tableFromIPC` in the Arrow JS bindings.
 #[wasm_bindgen(js_name = readParquet)]
 #[cfg(feature = "reader")]
-pub fn read_parquet(parquet_file: Vec<u8>) -> WasmResult<Uint8Array> {
+pub fn read_parquet(parquet_file: Vec<u8>) -> WasmResult<Vec<u8>> {
     assert_parquet_file_not_empty(parquet_file.as_slice())?;
-
-    let buffer = crate::arrow1::reader::read_parquet(parquet_file)?;
-    copy_vec_to_uint8_array(buffer)
+    Ok(crate::arrow1::reader::read_parquet(parquet_file)?)
 }
 
 /// Write Arrow data to a Parquet file using the [`arrow`](https://crates.io/crates/arrow) and
@@ -59,11 +56,13 @@ pub fn read_parquet(parquet_file: Vec<u8>) -> WasmResult<Uint8Array> {
 pub fn write_parquet(
     arrow_file: &[u8],
     writer_properties: Option<crate::arrow1::writer_properties::WriterProperties>,
-) -> WasmResult<Uint8Array> {
+) -> WasmResult<Vec<u8>> {
     let writer_props = writer_properties.unwrap_or_else(|| {
         crate::arrow1::writer_properties::WriterPropertiesBuilder::default().build()
     });
 
-    let buffer = crate::arrow1::writer::write_parquet(arrow_file, writer_props)?;
-    copy_vec_to_uint8_array(buffer)
+    Ok(crate::arrow1::writer::write_parquet(
+        arrow_file,
+        writer_props,
+    )?)
 }
