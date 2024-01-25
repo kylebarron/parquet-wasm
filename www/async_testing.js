d1b0237c00bab82d8c9a7ab5c5617475e99055fe
Nicholas Roberts <hydrogenplustime@gmail.com>
2024-01-26 05:41:37 +1100

    Arrow 1 struct-based API + concurrent streams + column selections + File reader (#407)
    * wip improved arrow1 async api
    
    * remove async_trait
    
    * Correct off by 1 byte range error, add concurrent stream method, batch size and column selection builder methods
    
    * Add JS File reader struct (without generic trait)
    
    * Shift shared IO into lightly-generic trait
    
    * Run clippy fix
    
    * cargo fmt
    
    * remove closure
    
    * camel case
    
    * Update src/common/fetch.rs
    
    * remove commented code
    
    * Add safety docs, avoid unsafe impl Sync
    
    * /very/ basic manual test of AsyncParquetLocalFile
    
    * Run cargo fmt, check cargo clippy, rip out unnecessary Arc<Mutex<>>
    
    ---------
    
    Co-authored-by: Kyle Barron <kylebarron2@gmail.com>
    Co-authored-by: Kyle Barron <kyle@developmentseed.org>

--- www/async_testing.js
+++ www/async_testing.js
@@ -1,13 +1,36 @@
 import * as arrow from "@apache-arrow/es2015-cjs/Arrow.dom";
 import * as wasm from "parquet-wasm/bundler/arrow2";
-
+import * as wasmArrow1 from "parquet-wasm/bundler/arrow1";
 wasm.setPanicHook();
 window.wasm = wasm;
 // const url = 'https://raw.githubusercontent.com/opengeospatial/geoparquet/main/examples/example.parquet';
 const url =
   "https://raw.githubusercontent.com/kylebarron/parquet-wasm/main/tests/data/2-partition-brotli.parquet";
 window.url = url;
-
+ReadableStream.prototype[Symbol.asyncIterator] = async function* () {
+    const reader = this.getReader()
+    try {
+        while (true) {
+            const { done, value } = await reader.read()
+            if (done) return
+            yield value
+        }
+    }
+    finally {
+        reader.releaseLock()
+    }
+}
+async function readParquetFile() {
+  const blobResult = (await fetch(url)).blob();
+  // a bit pointless, but definitely a file.
+  const simulatedFile = new File([blobResult], '2-partition-brotli.parquet', {type: 'application/vnd.apache.parquet'})
+  const instance = await new wasmArrow1.AsyncParquetLocalFile(simulatedFile);
+  for await (const chunk of instance.stream()) {
+    console.log(chunk);
+    console.log(chunk.length);
+    chunk.free();
+  }
+}
 async function main() {
   console.log("hello world");
   const resp = await fetch(url, { method: "HEAD" });
@@ -34,7 +57,9 @@ async function main() {
   const table = new arrow.Table(tables);
   window.table = table;
   console.log("table", table);
-
+  console.log('file IO');
+  await readParquetFile();
+  console.log('file IO');
   console.log("end of js");
 }
 
