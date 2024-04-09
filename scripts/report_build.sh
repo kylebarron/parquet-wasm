d8d08a9b7ac4896c0c2ffc1d3849d666af66fc57
Nicholas Roberts <nicholas.roberts.au@gmail.com>
2024-04-09 23:45:11 +1000

    Address report_build out of date reference to arrow1

--- scripts/report_build.sh
+++ scripts/report_build.sh
@@ -6,25 +6,24 @@ wasm-pack build \
   --release \
   --no-pack \
   --out-dir report_pkg/slim \
-  --out-name arrow1 \
-  --target web \
-  --features=arrow1 &
+  --out-name parquet_wasm \
+  --target web
 echo "Building arrow-rs sync"
 wasm-pack build \
   --release \
   --no-pack \
   --out-dir report_pkg/sync \
-  --out-name arrow1 \
+  --out-name parquet_wasm \
   --target web \
-  --features={arrow1,reader,writer,all_compressions} &
+  --features={reader,writer,all_compressions} &
 
 echo "Building arrow-rs async_full"
 wasm-pack build \
   --release \
   --no-pack \
   --out-dir report_pkg/async_full \
-  --out-name arrow1 \
+  --out-name parquet_wasm \
   --target web \
-  --features={arrow1,reader,writer,all_compressions,async} &
+  --features={reader,writer,all_compressions,async} &
 
 wait;
